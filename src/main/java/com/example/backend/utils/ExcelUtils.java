d1661647b31648b423c3e0ba2785813e1a3507e1
sykyyds <2536320349@qq.com>
2023-06-27 17:34:29 +0800

    编码测试

--- src/main/java/com/example/backend/utils/ExcelUtils.java
+++ src/main/java/com/example/backend/utils/ExcelUtils.java
@@ -13,20 +13,21 @@ import java.util.List;
 public class ExcelUtils {
 
     public static void createExcel(List<String> data, String filePath) throws IOException {
-        //创建一个XSSFWorkbook对象，表示一个Excel工作簿
+        // 创建一个XSSFWorkbook对象，表示一个Excel工作簿
         Workbook workbook = new XSSFWorkbook();
-        //通过workbook.createSheet("Sheet1")方法创建名为"Sheet1"的工作表
+
+        // 通过workbook.createSheet("Sheet1")方法创建名为"Sheet1"的工作表
         Sheet sheet = workbook.createSheet("Sheet1");
 
-        //循环遍历数据列表，逐行创建工作表的行,单元格
+        // 循环遍历数据列表，逐行创建工作表的行和单元格
         int rowCount = 0;
-
         for (String rowData : data) {
             Row row = sheet.createRow(rowCount++);
             int columnCount = 0;
             Cell cell = row.createCell(columnCount++);
             cell.setCellValue(rowData);
         }
+
 
         try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
             workbook.write(outputStream);
