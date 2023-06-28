742c283ac714873f36d1c43f783366cb869444e9
WorldOneTwo <2197248717>
2023-06-28 17:27:25 +0800

    完善注册功能

--- src/main/java/com/example/backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/backend/service/impl/UserServiceImpl.java
@@ -55,42 +55,51 @@ public class UserServiceImpl implements UserService {
      * */
     @Override
     public int register(Map<String, Object> params) {
-        // 获得传递参数
-        User user = new User();
-        user.setName((String) params.get("name"));
-        user.setPassword((String) params.get("password"));
-        user.setAge((int) params.get("age"));
-        user.setPhoneNumber((String) params.get("phoneNumber"));
-        user.setSex((int) params.get("sex"));
-        user.setType((int) params.get("type"));
+        try {
+            // 获得传递参数
+            User user = new User();
+            user.setName((String) params.get("name"));
+            user.setPassword((String) params.get("password"));
+            user.setAge((int) params.get("age"));
+            user.setPhoneNumber((String) params.get("phoneNumber"));
+            user.setSex((int) params.get("sex"));
+            user.setType((int) params.get("type"));
 
-        String inviteCode = (String) params.get("invite_code");
+            String inviteCode = (String) params.get("invite_code");
 
-        //判断参数是否满足条件
-        if (user.getName() == null || user.getName().equals("")) {
-            throw new OSException(OSExceptionEnum.USER_PARAM_NULL);
-        }
-        if (userMapper.selectOneByName(user.getName()) != null) {
-            throw new OSException(OSExceptionEnum.USER_EXISTS);
-        }
-        if (user.getAge() <= 0 || user.getSex() < 0 || user.getSex() > 1 || user.getType() < 0 || user.getType() > 2) {
-            throw new OSException(OSExceptionEnum.PARAM_ERROR);
-        }
-        if (user.getPhoneNumber() == null || user.getPhoneNumber().equals("")) {
-            throw new OSException(OSExceptionEnum.PARAM_ERROR);
-        }
-        // 判断邀请码合法性
-        if (user.getType() == 1 && !validInviteCode(inviteCode)) {
-            throw new OSException(OSExceptionEnum.INVITE_CODE_ERROR);
-        }
-        // 判断密码合法性
-        if (!validPassword(user.getPassword())) {
-            throw new OSException(OSExceptionEnum.PASSWORD_ERROR);
-        }
-        // 持久化数据
-        int result = userMapper.insert(user);
+            //判断参数是否满足条件
+            if (user.getName() == null || user.getName().equals("")) {
+                throw new OSException(OSExceptionEnum.USER_PARAM_NULL);
+            }
+            if (userMapper.selectOneByName(user.getName()) != null) {
+                throw new OSException(OSExceptionEnum.USER_EXISTS);
+            }
+            if (user.getAge() <= 0 || user.getSex() < 0 || user.getSex() > 1 || user.getType() < 0 || user.getType() > 2) {
+                throw new OSException(OSExceptionEnum.PARAM_ERROR);
+            }
+            if (user.getPhoneNumber() == null || user.getPhoneNumber().equals("")) {
+                throw new OSException(OSExceptionEnum.PARAM_ERROR);
+            }
+            // 判断邀请码合法性
+            if (user.getType() == 1 && !validInviteCode(inviteCode)) {
+                throw new OSException(OSExceptionEnum.INVITE_CODE_ERROR);
+            }
+            // 判断密码合法性
+            if (!validPassword(user.getPassword())) {
+                throw new OSException(OSExceptionEnum.PASSWORD_ERROR);
+            }
+            // 持久化数据
+            int result = userMapper.insert(user);
 
-        return result;
+            return result;
+        }
+        catch (OSException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new OSException("存在错误");
+        }
+
     }
 
     /**
