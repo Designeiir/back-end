92d75f0d55f949527e9db29d8bf85a3dd10719e5
Nowherechan <zzxy233@gmail.com>
2023-07-08 16:36:24 +0800

    refactor VideoUploadController to DocResourceController, move getChapter and getLecture into LectureAndChapterController

---
+++ src/main/java/com/example/backend/controller/DocResourceController.java
@@ -0,0 +1,129 @@
+package com.example.backend.controller;
+import com.example.backend.annotation.PassToken;
+import com.example.backend.entity.ResultInfo;
+import com.example.backend.service.ChapterService;
+import com.example.backend.service.LectureService;
+import com.example.backend.utils.ResultInfoUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@RestController
+@CrossOrigin
+public class DocResourceController {
+
+    @Autowired
+    private LectureService lectureService;
+
+    @Autowired
+    private ChapterService chapterService;
+
+    //处理文件，并存链接到数据库
+    @PassToken
+    @Transactional
+    @PostMapping("/upload")
+    public ResultInfo uploadVideo(
+            //@RequestPart("file") MultipartFile file,
+            @RequestPart("file") MultipartFile file,
+            @RequestParam("courseId") String courseId,
+            @RequestParam("chapterId") String chapterId,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description
+    ) {
+        // 构建视频存储路径
+        String storagePath = "E:/desktop/视频存放/" + courseId + "/";
+
+        try {
+            // 创建目录（如果不存在）
+            Path directoryPath = Paths.get(storagePath);
+            Files.createDirectories(directoryPath);
+
+            // 构建目标文件路径
+            String fileName = file.getOriginalFilename();
+            Path targetPath = directoryPath.resolve(fileName);
+
+            // 存储文件
+            Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+            // 获取文件类型
+            String type = file.getContentType();
+
+           /* // 获取videoInfo中的name和description
+            String name = videoInfo.getName();
+            String description = videoInfo.getDescription();*/
+
+            //文件链接
+            String url = "http://localhost:10100/uploads/" + courseId + "/" + fileName;
+
+            //向数据库传输
+            int id = Integer.parseInt(courseId);
+            int order = Integer.parseInt(chapterId);
+            lectureService.lectureInserted(id, order, url, type, name, description);
+
+
+            // 返回视频的访问链接或文件保存路径
+
+            return ResultInfoUtils.success("上传成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 处理文件存储失败的情况
+            return ResultInfoUtils.error(500, "文件上传失败");
+        }
+    }
+
+    //根据链接返回文件数据
+    @PassToken
+    @GetMapping("/uploads")
+    public ResponseEntity<Resource> getFile(@RequestParam String courseId, @RequestParam String fileName, @RequestParam String filetype) {
+        // 根据 courseId 和 fileName 获取文件路径
+        String filePath = "E:/desktop/视频存放/" + courseId + "/" + fileName;
+
+
+        // URL 编码文件名
+        try {
+            fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // 检查文件是否存在
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            // 读取文件内容
+            Resource resource = new FileSystemResource(file);
+
+            // 设置响应头
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentDisposition(ContentDisposition.builder("inline").filename(fileName).build());
+            headers.set(HttpHeaders.CONTENT_TYPE, filetype);
+
+            // 返回文件内容
+            return ResponseEntity.ok().headers(headers).body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 处理读取文件失败的情况
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/example/backend/controller/LectureAndChapterController.java
@@ -0,0 +1,66 @@
+package com.example.backend.controller;
+
+import com.example.backend.annotation.PassToken;
+import com.example.backend.entity.ResultInfo;
+import com.example.backend.service.ChapterService;
+import com.example.backend.service.LectureService;
+import com.example.backend.utils.ResultInfoUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class LectureAndChapterController {
+
+    @Autowired
+    private LectureService lectureService;
+
+    @Autowired
+    private ChapterService chapterService;
+
+    @PassToken
+    @GetMapping("/getLecture")
+    public ResultInfo getLecture(
+            @RequestParam("cid")int cid
+    ){
+        return ResultInfoUtils.success(lectureService.getLecture(cid));
+    }
+
+    @PassToken
+    @GetMapping("/getChapter")
+    public ResultInfo getChapter(
+            @RequestParam("cid")int cid
+    ){
+        return ResultInfoUtils.success(chapterService.getChapter(cid));
+    }
+
+    @PassToken
+    @GetMapping("/addChapter")
+    public ResultInfo addChapter(
+            @RequestParam("cid")int cid,
+            @RequestParam("no")int no,
+            @RequestParam("name")String name
+    ){
+        return ResultInfoUtils.success(chapterService.addChapter(cid, no, name));
+    }
+
+    @PassToken
+    @GetMapping("/deleteChapter")
+    public ResultInfo deleteChapter(
+            @RequestParam("cid")int cid,
+            @RequestParam("no")int no
+    ){
+        return ResultInfoUtils.success(chapterService.deleteChapter(cid, no));
+    }
+
+    @PassToken
+    @GetMapping("/deleteLecture")
+    public ResultInfo deleteLecture(
+            @RequestParam("lid")int lid
+    ){
+        return ResultInfoUtils.success(lectureService.deleteLectureByLid(lid));
+    }
+}
